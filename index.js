819ec4e7e1ef9d7fa56cea094d052afbd52591a6
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-10 13:15:41 -0300

    Update API endpoint and add CORS middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cors = require('cors');
 
 app.listen(3000, console.log("Servidor encendido"));
+
+app.use(cors());
 
 app.use(express.json());
 
